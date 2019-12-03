3e3a983d9b94a2de929b81bca87c171201450906
Isaiah Bush <isaiah@DN0a245704.SUNet>
2019-12-03 13:10:27 -0800

    Supports visualization using the mock data. Currently has bugs concerning typecasting warnings. Supports clicking on header for minimization of tables. g Please enter the commit message for your changes. Lines starting

--- src/client/components/Diagram.tsx
+++ src/client/components/Diagram.tsx
@@ -1,6 +1,123 @@
 import * as React from 'react';
 import * as joint from 'jointjs';
 
+const FONT_SIZE = 9;
+const MOCK_DATA = {
+    processes: [{
+        pid: 123,
+        ppid: 122,
+        pgid: 10,
+        threads: [{
+            tid: 123,
+            state: 'S (sleeping)',
+            cpusAllowed: [0, 1, 2, 3, 4, 5, 6, 7],
+            pendingSignals: [
+                'SIGINT',
+            ],
+            blockedSignals: [
+                'SIGINT',
+            ],
+            ignoredSignals: [
+                'SIGTSTP',
+            ],
+        }],
+        fds: {
+            0: {
+                file: 'someid0',
+                closeOnExec: false,
+            },
+            1: {
+                file: 'someid0',
+                closeOnExec: false,
+            },
+            2: {
+                file: 'someid0',
+                closeOnExec: false,
+            },
+            3: {
+                file: 'someid3',
+                closeOnExec: true,
+            },
+            4: {
+                file: 'someid3',
+                closeOnExec: false,
+            },
+        },
+    }, {
+        pid: 123,
+        ppid: 122,
+        pgid: 10,
+        threads: [{
+            tid: 123,
+            state: 'S (sleeping)',
+            cpusAllowed: [0, 1, 2, 3, 4, 5, 6, 7],
+            pendingSignals: [
+                'SIGINT',
+            ],
+            blockedSignals: [
+                'SIGINT',
+            ],
+            ignoredSignals: [
+                'SIGTSTP',
+            ],
+        }],
+        fds: {
+            0: {
+                file: 'someid0',
+                closeOnExec: false,
+            },
+            1: {
+                file: 'someid0',
+                closeOnExec: false,
+            },
+            2: {
+                file: 'someid0',
+                closeOnExec: false,
+            },
+            3: {
+                file: 'someid3',
+                closeOnExec: true,
+            },
+            4: {
+                file: 'someid3',
+                closeOnExec: false,
+            },
+        },
+    }],
+    openFiles: {
+        someid0: {
+            position: 123, // offset in file
+            flags: ['O_WRONLY'],
+            refcount: 3,
+            vnode: 'vnode0',
+        },
+        someid3: {
+            position: 123, // offset in file
+            flags: ['O_RDONLY'],
+            refcount: 2,
+            vnode: 'vnode1',
+        },
+    },
+    vnodes: {
+        vnode0: {
+            name: '/dev/pts/2',
+            refcount: 1,
+            inode: {
+                mode: '0700',
+                owner: 'cplayground',
+            },
+        },
+        vnode1: {
+            name: '/path/to/file',
+            refcount: 1,
+            inode: {
+                mode: '0755',
+                owner: 'root',
+            },
+        },
+    },
+};
+
 type DiagramProps = {
 }
 
@@ -23,31 +140,258 @@ class Diagram extends React.Component<DiagramProps> {
             height: this.divRef.current.getBoundingClientRect().height,
             gridSize: 1,
         });
+        paper.setInteractivity({ elementMove: false });
 
-        const rect = new joint.shapes.standard.Rectangle();
-        rect.position(100, 30);
-        rect.resize(100, 40);
-        rect.attr({
-            body: {
-                fill: 'blue',
-            },
-            label: {
-                text: 'Hello',
-                fill: 'white',
-            },
+        // TODO: figure out type casting issues
+        // TODO: debug 'Missing return type on function' issues
+        const { processes } = MOCK_DATA;
+        const { openFiles } = MOCK_DATA;
+        const { vnodes } = MOCK_DATA;
+
+        const links: Array<joint.shapes.standard.Link> = [];
+        const cells: Array<joint.shapes.standard.Rectangle> = [];
+        const vnodeTable: {[index: string]: joint.shapes.standard.Rectangle} = {};
+        const fileTableIndeces: {[index: string]: joint.shapes.standard.HeaderedRectangle} = {};
+        const inodes: Array<joint.shapes.standard.Rectangle> = [];
+
+        // VNODE TABLE
+        function generateVnode() {
+            let xPosition = 0;
+            const yPosition = 250;
+            const vnodeKeys = Object.keys(vnodes);
+            vnodeKeys.forEach((vKey) => {
+                const vnodeRect = new joint.shapes.standard.Rectangle();
+                const v = vnodes[vKey];
+                vnodeRect.resize(100, 80);
+                vnodeRect.position(xPosition, yPosition);
+                vnodeRect.attr({
+                    body: {
+                        strokeWidth: 1,
+                    },
+                    label: {
+                        // TODO:scale box if needed to fit longer text
+                        text: `${v.name}\nrefcount: ${v.refcount}\ninode:`,
+                        fontSize: FONT_SIZE,
+                        fontFamily: 'Courier',
+                        textAnchor: 'front',
+                        'ref-x': -45,
+                        'ref-y': -20,
+                    },
+                });
+                // vnodeRect.addTo(graph);
+
+                const inode = new joint.shapes.standard.Rectangle();
+                inode.resize(80, 40);
+                inode.position(xPosition + 5, yPosition + 35);
+                inode.attr({
+                    body: {
+                        strokeWidth: 1,
+                        fill: 'blue',
+                        fillOpacity: 0.1,
+                    },
+                    label: {
+                        text: `${v.inode.mode} \n${v.inode.owner}`,
+                        fontSize: FONT_SIZE,
+                        fontFamily: 'Courier',
+                        textAnchor: 'front',
+                        'ref-x': -35,
+                    },
+                });
+                cells.push(inode);
+                // inode.addTo(graph);
+                vnodeRect.embed(inode);
+                vnodeTable[vKey] = vnodeRect;
+                xPosition += 100;
+                // fileCells.push(dict);
+            });
+        }
+        // FILETABLE
+        function generateFileTable() {
+            let xPosition = 0;
+            const yPosition = 140;
+            const fileKeys = Object.keys(openFiles);
+            fileKeys.forEach((key) => {
+                const fileRect = new joint.shapes.standard.HeaderedRectangle();
+                const file = openFiles[key];
+                fileRect.resize(100, 100);
+                fileRect.position(xPosition, yPosition);
+                fileRect.attr({
+                    body: {
+                        strokeWidth: 1,
+                    },
+                    header: {
+                        strokeWidth: 1,
+                    },
+                    headerText: {
+                        text: `${key}`,
+                        fontSize: FONT_SIZE,
+                        fontFamily: 'Courier',
+
+                    },
+                    bodyText: { // TODO: parse flags to list multiple, scale box if needed
+                        text: `cursor: ${file.position} \nrefcount: ${file.refcount}\nflags: ${file.flags.toString()}`,
+                        fontSize: FONT_SIZE,
+                        fontFamily: 'Courier',
+                        textAnchor: 'front',
+                        'ref-x': -48,
+                        'ref-y': -20,
+                    },
+                });
+                // fileRect.addTo(graph);
+                const inode = new joint.shapes.standard.Rectangle();
+                inode.resize(15, 15);
+                inode.position(xPosition + 5, yPosition + 70);
+                inode.attr({
+                    body: {
+                        strokeWidth: 1,
+                        fill: 'yellow',
+                        fillOpacity: 0.5,
+                    },
+                });
+                const link = new joint.shapes.standard.Link();
+                link.source(inode);
+                link.target(vnodeTable[file.vnode]);
+                link.attr({
+                    line: {
+                        connection: true,
+                        stroke: 'green',
+                    },
+                });
+                links.push(link);
+
+                // inode.addTo(graph);
+                fileRect.embed(inode); // TODO don't let inode leave fileRect
+                inodes.push(inode);
+
+                fileTableIndeces[key] = fileRect;
+                xPosition += 100;
+                // fileCells.push(dict);
+            });
+        }
+
+        function generateProcesses() {
+            let xPosition = 0;
+            const yPosition = 30;
+            processes.forEach((process) => {
+                const rect2 = new joint.shapes.standard.HeaderedRectangle();
+                rect2.resize(200, 100);
+                rect2.position(xPosition, yPosition);
+                rect2.attr({
+                    body: {
+                        strokeWidth: 1,
+                    },
+                    header: {
+                        fill: 'yellow',
+                        fillOpacity: 1,
+                        strokeWidth: 1,
+                    },
+                    headerText: {
+                        text: `pid: ${process.pid} \nppid: ${process.ppid} `,
+                        fontSize: FONT_SIZE,
+                        fontFamily: 'Courier',
+                        textAnchor: 'left',
+                        'ref-x': -90,
+                    },
+                });
+                let offset = 10;
+                cells.push(rect2);
+
+                // FD TABLE
+
+                // TODO: make fd table into function
+
+                Object.keys(process.fds).forEach((fdKey) => {
+                    const fd = new joint.shapes.standard.Rectangle();
+                    const fileKey = process.fds[parseInt(fdKey, 10)].file;
+                    fd.resize(20, 20);
+                    fd.position(xPosition + offset, yPosition + 50);
+                    fd.attr({
+                        body: {
+                            strokeWidth: 1,
+                        },
+                        label: {
+                            text: `${fdKey} `,
+                            fontSize: FONT_SIZE,
+                            fontFamily: 'Courier',
+                            textAnchor: 'middle',
+                        },
+                    });
+
+                    offset += 20;
+                    cells.push(fd);
+                    rect2.embed(fd);
+
+                    const link = new joint.shapes.standard.Link();
+                    link.source(fd);
+                    link.target(fileTableIndeces[fileKey]);
+                    link.attr({
+                        line: {
+                            connection: true,
+                            stroke: 'green',
+                        },
+                    });
+                    links.push(link);
+                });
+                // graph.addCells(cells);
+
+                // graph.addCells(links);
+
+                // rect2.addTo(graph);
+
+                xPosition += 250;
+            });
+        }
+
+        function draw() {
+            Object.keys(vnodeTable).forEach((vnodeKey) => {
+                graph.addCell(vnodeTable[vnodeKey]);
+            });
+            Object.keys(fileTableIndeces).forEach((fileTableKey) => {
+                graph.addCell(fileTableIndeces[fileTableKey]);
+            });
+            graph.addCells(cells);
+            graph.addCells(inodes);
+            graph.addCells(links);
+        }
+
+        generateVnode();
+        generateFileTable();
+        generateProcesses();
+        draw();
+
+        // Click on header to minimize it
+        paper.on('element:pointerdblclick', (ElementView) => {
+            const currentElem = ElementView.model;
+            let visibility = 'hidden';
+            if (currentElem.attr('body/visibility') === 'hidden') {
+                visibility = 'visible';
+            }
+            if (currentElem instanceof joint.shapes.standard.HeaderedRectangle) {
+                const children = currentElem.getEmbeddedCells();
+                const used: Array<string> = [];
+                children.forEach((child) => {
+                    child.attr('body/visibility', visibility);
+                    if (visibility === 'hidden') {
+                        links.forEach((link) => {
+                            if (link.attributes.source.id === child.id) {
+                                link.source(currentElem, { selector: 'header' });
+                            }
+                        });
+                    } else {
+                        links.forEach((link) => {
+                            if (link.attributes.source.id === currentElem.id
+                                && !used.includes(child.cid)) {
+                                link.source(child, { selector: 'header' });
+                                used.push(child.cid);
+                            }
+                        });
+                    }
+                });
+                currentElem.attr('body/visibility', visibility);
+            }
         });
-        rect.addTo(graph);
-
-        const rect2 = rect.clone() as joint.shapes.standard.Rectangle;
-        rect2.translate(300, 0);
-        rect2.attr('label/text', 'World!');
-        rect2.addTo(graph);
-
-        const link = new joint.shapes.standard.Link();
-        link.source(rect);
-        link.target(rect2);
-        link.addTo(graph);
     }
+
 
     componentWillUnmount(): void {
         // TODO: put something here
